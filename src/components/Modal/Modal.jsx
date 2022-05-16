f6e66c6e636af3abfa3cc0516e841f707c8cd411
Oleg Muraviov <95213060+OlegMuraviov@users.noreply.github.com>
2022-05-16 16:27:43 +0300

    fixed

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,44 +1,39 @@
 import { createPortal } from 'react-dom';
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from './Modal.module.css';
+import { useEffect } from 'react';
 
 const modalRoot = document.getElementById('modal');
 
-class Modal extends Component {
-  handleEscape = event => {
+const Modal = ({ modalData, toggleModal }) => {
+  const handleEscape = event => {
     if (event.code === 'Escape') {
-      this.props.toggleModal();
+      toggleModal();
     }
   };
+  useEffect(() => {
+    window.addEventListener('keydown', handleEscape);
+    return () => {
+      window.removeEventListener('keydown', handleEscape);
+    };
+  }, []);
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscape);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscape);
-  }
-
-  render() {
-    const { modalData, toggleModal } = this.props;
-    return createPortal(
-      <div
-        onClick={() => {
-          toggleModal();
-        }}
-        className={s.overlay}
-      >
-        <img
-          className={s.modal}
-          src={modalData.largeImageURL}
-          alt={modalData.tags}
-        />
-      </div>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <div
+      onClick={() => {
+        toggleModal();
+      }}
+      className={s.overlay}
+    >
+      <img
+        className={s.modal}
+        src={modalData.largeImageURL}
+        alt={modalData.tags}
+      />
+    </div>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
   modalImage: PropTypes.object,
